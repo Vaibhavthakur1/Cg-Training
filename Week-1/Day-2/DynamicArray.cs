388d93cee404daf912a70cd68c300504a9a298d6
Vaibhav Singh <vaibhavsingh99221@gmail.com>
2026-07-31 15:45:14 +0530

    Array assignments

---
+++ Week-1/Day-2/DynamicArray.cs
@@ -0,0 +1,87 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<int> dynamicArray(int n, List<List<int>> queries)
+    {
+        List<List<int>> arr=new List<List<int>>();
+        
+        for(int i=0;i<n;i++){
+            arr.Add(new List<int>());
+        }
+        int lastAnswer=0;
+        List<int> answer =new List<int>();
+        
+        foreach(List<int>query in queries){
+            int type=query[0];
+            int x=query[1];
+            int y= query[2];
+            
+            int idx=(x^lastAnswer)%n;
+            
+            if (type == 1)
+        {
+            arr[idx].Add(y);
+        }
+        else if (type == 2)
+        {
+            lastAnswer = arr[idx][y % arr[idx].Count];
+            answer.Add(lastAnswer);
+        }
+    }
+
+    return answer;
+     }
+}
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int q = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 0; i < q; i++)
+        {
+            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+        }
+
+        List<int> result = Result.dynamicArray(n, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
--- Week-1/Day-2/QueueImplementation/obj/Debug/net10.0/QueueImplementation.AssemblyInfo.cs
+++ Week-1/Day-2/QueueImplementation/obj/Debug/net10.0/QueueImplementation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueImplementation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423fdd4df9261db6bac7da66dabb34747cb2938a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a740e76c31a061f7547eaacdd774ef6f5fbd1d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueImplementation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueImplementation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
